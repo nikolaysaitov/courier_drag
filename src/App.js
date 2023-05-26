99ccf02b36837c3badd2ca462630caf3411f2716
nikolay <saitov@impermebel.ru>
2023-05-26 13:51:11 +0300

    26_05_2

--- src/App.js
+++ src/App.js
@@ -2,8 +2,17 @@ import * as React from "react";
 import { useState } from "react";
 import { Reorder } from "framer-motion";
 import { Item } from "./Item";
-import { Switch, Button, Modal, Result } from "antd";
-import Icon, { InfoCircleOutlined, CheckOutlined } from "@ant-design/icons";
+import { Switch, Button, Modal, Result, Dropdown, Space, message } from "antd";
+import { DownOutlined, PhoneOutlined, RedoOutlined } from "@ant-design/icons";
+
+import { TileLayer } from "react-leaflet/TileLayer";
+import { MapContainer } from "react-leaflet";
+import { Popup } from "react-leaflet/Popup";
+import { Marker } from "react-leaflet/Marker";
+
+import IconTruck from "../src/icons/IconTruck";
+import IconFinish from "../src/icons/IconFinish";
+
 import "./App.css";
 const initialItems = [
   {
@@ -21,12 +30,18 @@ const initialItems = [
   { id: 5, order: 108340, text: "Владивосток, ул Ладыгина, д 1, офис 12" },
   { id: 6, order: 16340, text: "Владивосток, ул Давыдова, д 111, офис 7/12" },
   { id: 7, order: 1086740, text: "Владивосток, ул Ленина, д 8, офис 17" },
-  { id: 8, order: 18340, text: "Владивосток, ул Петрова, д 7, офис 6" },
+  { id: 8, order: 18340, text: "Владивосток, ул Ризеншнауцера-Циммер-Вандершпигеля, д 7, офис 6" },
+  { id: 9, order: 14340, text: "Владивосток, ул Адыгина, д 9, офис 142" },
+  { id: 10, order: 1670, text: "Владивосток, ул Ли, д 191, офис 7912" },
+  { id: 11, order: 176740, text: "Владивосток, ул Хренова, д 24, офис 14" },
+  { id: 12, order: 1640, text: "Владивосток, ул Парк Авеню, д 88, офис 36" },
 ];
 
-export default function App() {
-  const [items, setItems] = useState(initialItems);
 
+export default function App() {
+  const [adress, setAdress] = useState(initialItems);
+console.log("initialItems:", initialItems)
+console.log("adress:", adress)
   //Возможность изменить порядок вкл/выкл
   const [drag, setDrag] = useState(false);
 
@@ -52,54 +67,147 @@ export default function App() {
     setOpen(false);
   };
 
+  // Свайп вверх вниз блока с маршрутами
+  const [swipeDirection, setSwipeDirection] = useState(null);
+
+  const handleTouchStart = (event) => {
+    setSwipeDirection(null);
+    const touch = event.touches[0];
+    setSwipeDirection({ x: touch.clientX, y: touch.clientY });
+  };
+
+  const handleTouchEnd = (event) => {
+    const touch = event.changedTouches[0];
+    const xDiff = touch.clientX - swipeDirection.x;
+    const yDiff = touch.clientY - swipeDirection.y;
+    if (Math.abs(xDiff) > Math.abs(yDiff)) {
+      if (xDiff > 0) {
+        // swipe right
+      } else {
+        // swipe left
+      }
+    } else {
+      if (yDiff > 0) {
+        // swipe down
+        setSwipeDirection("down");
+      } else {
+        // swipe up
+        setSwipeDirection("up");
+      }
+    }
+  };
+// Меню аккаунта водителя
+  const handleMenuClick = (e) => {
+    console.log("click", e);
+  };
+  const items = [
+    {
+      label: "Изменить пароль",
+      key: "1",
+      // icon: <UserOutlined />,
+    },
+    {
+      label: "Выйти",
+      key: "2",
+      // icon: <UserOutlined />,
+    },
+    
+  ];
+  const menuProps = {
+    items,
+    onClick: handleMenuClick,
+  };
+
   return (
     <>
-    <div className="container__buttons">
-
-      <div className="check__drag">
-        <p className="switch__title">Изменить порядок маршрута</p>
-        <Switch size="small" checked={drag} onChange={onChange} />
-      </div>
-      <div className="check__drag">
-        {/* <p className="switch__title">Начать поездку</p> */}
-        <Button type="primary" onClick={showModal} disabled={drag}>
-          Выехал
-        </Button>
-      </div>
-
-    </div>
-      
-      <Modal
-        open={open}
-        title="Внимание!"
-        onOk={handleOk}
-        onCancel={handleCancel}
-        footer={[
-          <Button key="back" onClick={handleCancel}>
-            Закрыть
-          </Button>,
-          <Button key="submit" type="primary" loading={loading} onClick={handleOk}>
-            Начать
-          </Button>,
+      <MapContainer center={[43.0956391, 131.9037986]} zoom={13} scrollWheelZoom={false}>
+        <Dropdown menu={menuProps} >
+          <Button style={{ zIndex: "99999", position: "absolute", right: "10px", top: "10px", outlineStyle: "none" }}>
+            <Space>
+              Мистер Вандершпигель
+              <DownOutlined />
+            </Space>
+          </Button>
           
-        ]}
-      ><Result
-      status="warning"
-      style={{fontSize: '13px'}}
-      title="Нажимая кнопку 'Начать', клиенты получат уведомления, что вы выехали. Желаете начать маршрут?"
-      className="result__go"
-    />
-     
+        </Dropdown>
+        <a href="tel:+79999999999"><PhoneOutlined style={{fontSize: "18px"}} className="phone__dispatcher"/></a>
+        
+        <a href="#" onClick={() => window.location.reload()}>
+        <RedoOutlined style={{fontSize: "18px"}} className="phone__update"/>
+</a>
+        
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
 
-      </Modal>
+        <Marker position={[43.0956391, 131.9037986]}>
+        <Popup>
+        {/* <a target="_blank" rel="noopener noreferrer" href="yandexnavi://build_route_on_map?lat_to=43.13002&lon_to=131.92016"> */}
+        <a target="_blank" rel="noopener noreferrer" href="dgis://2gis.ru/routeSearch/rsType/car/to/30.149939,59.849767">
+        Владивосток, ул Ризеншнауцера-Циммер-Вандершпигеля, д 7, офис 6
+        </a>
+      </Popup>
+        </Marker>
 
-      <Reorder.Group axis="y" onReorder={setItems} values={items}>
-        {items.map((item, index) => (
-          <Item key={item.id} item={item} index={index} drag={drag} />
-        ))}
-      </Reorder.Group>
+        <Marker position={[43.0966391, 131.9137986]} icon={IconTruck}></Marker>
+        <Marker position={[43.0926391, 131.9537986]} icon={IconFinish}></Marker>
+        
+      </MapContainer>
+      <div className="main__routes__block">
+        <div
+          className={
+            swipeDirection === "down"
+              ? "main__routes__block block d-flex flex-column align-items-center down"
+              : "main__routes__block block d-flex flex-column align-items-center"
+          }
+        >
+          <div className="button__swipe__div" onTouchStart={handleTouchStart} onTouchEnd={handleTouchEnd}>
+            <button className="swipe_line mt-2 mb-1">{/* <span className="arrow"></span> */}</button>
+          </div>
 
-
+          <div className="container__buttons" onTouchStart={handleTouchStart} onTouchEnd={handleTouchEnd}>
+            <div className="check__drag">
+              <p className="switch__title">Изменить порядок маршрута</p>
+              <Switch size="small" checked={drag} onChange={onChange} />
+            </div>
+            <div className="check__drag">
+              {/* <p className="switch__title">Начать поездку</p> */}
+              <Button type="primary" onClick={showModal} disabled={drag}>
+                Выехал
+              </Button>
+            </div>
+          </div>
+          <Modal
+            centered
+            open={open}
+            title="Внимание!"
+            onOk={handleOk}
+            onCancel={handleCancel}
+            style={{ zIndex: "9999" }}
+            footer={[
+              <Button key="back" onClick={handleCancel}>
+                Закрыть
+              </Button>,
+              <Button key="submit" type="primary" loading={loading} onClick={handleOk}>
+                Начать
+              </Button>,
+            ]}
+          >
+            <Result
+              status="warning"
+              style={{ fontSize: "13px" }}
+              title="Нажимая кнопку 'Начать', клиенты получат уведомления, что вы выехали. Желаете начать маршрут?"
+              className="result__go"
+            />
+          </Modal>
+          <Reorder.Group axis="y" onReorder={setAdress} values={adress} className="ul__routes">
+            {adress.map((item, index) => (
+              <Item key={item.id} item={item} index={index} drag={drag} />
+            ))}
+          </Reorder.Group>
+        </div>
+      </div>
     </>
   );
 }
--- src/Item.js
+++ src/Item.js
@@ -1,17 +1,18 @@
 import * as React from "react";
+
 import { useState } from "react";
 import { useMotionValue, Reorder, useDragControls } from "framer-motion";
 import { useRaisedShadow } from "./use-raised-shadow";
 import { ReorderIcon } from "./Icon";
 import { Col, Row, Button, Modal } from "antd";
-import Icon, { InfoCircleOutlined, CheckOutlined } from "@ant-design/icons";
+import Icon, { InfoCircleOutlined, CheckOutlined, GlobalOutlined } from "@ant-design/icons";
 
 export const Item = (props) => {
   const y = useMotionValue(0);
   const boxShadow = useRaisedShadow(y);
   const dragControls = useDragControls();
 
-//Модалка инфо:
+  //Модалка инфо:
   const [isModalOpenInfo, setIsModalOpenInfo] = useState(false);
   const showModalInfo = () => {
     setIsModalOpenInfo(true);
@@ -42,40 +43,56 @@ export const Item = (props) => {
 
   return (
     <>
-      <Reorder.Item value={props.item} id={props.item.id} style={{ boxShadow, y }} dragListener={props.drag} dragControls={dragControls}>
+      <Reorder.Item
+        value={props.item}
+        id={props.item.id}
+        style={{ boxShadow, y, backgroundColor: (props.item.id ===8 ? '#ff000036' : null) }}
+        dragListener={props.drag}
+        dragControls={dragControls}
+        
+      >
         <Row justify="space-between" align="middle">
           <Col span={2}>
             <p className="item_text_queue">{props.index + 1}</p>
           </Col>
-          <Col span={15}>
+          <Col span={13}>
             <p className="item_text">{props.item.text}</p>
           </Col>
-          <Col span={7} className="col__end">
+          <Col span={9} className="col__end">
             <CheckOutlined className="button__done" onClick={showModalDone} />
             <InfoCircleOutlined className="button__info" onClick={showModalInfo} />
+            <a href="dgis://2gis.ru/routeSearch/rsType/car/to/30.149939,59.849767">
+              <GlobalOutlined className="button__navi" />
+            </a>
+
             <ReorderIcon dragControls={dragControls} className="button__info" />
           </Col>
         </Row>
       </Reorder.Item>
-      <Modal title="Информация о заказе:" open={isModalOpenInfo} onOk={handleOkInfo} onCancel={handleCancelInfo} cancelText="Отмена">
+      <Modal centered title="Информация о заказе:" open={isModalOpenInfo} onOk={handleOkInfo} onCancel={handleCancelInfo} cancelText="Отмена">
         <p>Имя: Валентина Туманова</p>
-        <p>Телефон: 79846613788</p>
+        <p>Телефон: <a href="tel:+79999999999">79999999999</a></p>
         <p>Время заказа: 21 мая 2023 г. 3:15</p>
         <p>Место покупки: ИНТЕРНЕТ-МАРКЕТПЛЕЙС ОЗОН ДВ</p>
         <p>Комментарий: (НАЛ) OZON , ЗВОНОК ЗА ЧАС - 8 952 081 11 41 , доп. номер - 8 994 010 20 29 , Ботанический сад , частный дом</p>
       </Modal>
 
-      <Modal title="Подтверждение" open={isModalOpenDone} cancelText="Отмена" onCancel={handleCancelDone} footer={[
+      <Modal
+        centered
+        title="Заказ выполнен?"
+        open={isModalOpenDone}
+        cancelText="Отмена"
+        onCancel={handleCancelDone}
+        footer={[
           <Button key="back" onClick={handleCancelDone}>
             Закрыть
           </Button>,
           <Button key="submit" type="primary" loading={loading} onClick={handleOkDone}>
-            Выполнено!
+            Выполнен!
           </Button>,
-          
-        ]}>
+        ]}
+      >
         <p>Вы уверены?</p>
-        
       </Modal>
     </>
   );
