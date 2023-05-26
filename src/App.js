1e193914c178dde4b0cdab82b6303c7d394f4923
nikolay <saitov@impermebel.ru>
2023-05-26 08:50:22 +0300

    26.05

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-
 import * as React from "react";
 import { useState } from "react";
 import { Reorder } from "framer-motion";
 import { Item } from "./Item";
-import './App.css';
+import { Switch, Button, Modal, Result } from "antd";
+import Icon, { InfoCircleOutlined, CheckOutlined } from "@ant-design/icons";
+import "./App.css";
 const initialItems = [
   {
     id: 1,
@@ -17,17 +18,88 @@ const initialItems = [
     order: 110372,
     text: "Владивосток, ул Черняховского, д 421, кв 42",
   },
-  { id: 5, order: 108340, text: "Владивосток, ул Ладыгина, д 71, офис 1/12" },
+  { id: 5, order: 108340, text: "Владивосток, ул Ладыгина, д 1, офис 12" },
+  { id: 6, order: 16340, text: "Владивосток, ул Давыдова, д 111, офис 7/12" },
+  { id: 7, order: 1086740, text: "Владивосток, ул Ленина, д 8, офис 17" },
+  { id: 8, order: 18340, text: "Владивосток, ул Петрова, д 7, офис 6" },
 ];
 
 export default function App() {
   const [items, setItems] = useState(initialItems);
 
+  //Возможность изменить порядок вкл/выкл
+  const [drag, setDrag] = useState(false);
+
+  const onChange = (checked) => {
+    console.log(`switch to ${checked}`);
+    setDrag(checked);
+  };
+
+  // Модалка кнопки начала маршрута
+  const [loading, setLoading] = useState(false);
+  const [open, setOpen] = useState(false);
+  const showModal = () => {
+    setOpen(true);
+  };
+  const handleOk = () => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+      setOpen(false);
+    }, 1000);
+  };
+  const handleCancel = () => {
+    setOpen(false);
+  };
+
   return (
-    <Reorder.Group axis="y" onReorder={setItems} values={items}>
-      {items.map((item, index) => (
-        <Item key={item.id} item={item} index={index}/>
-      ))}
-    </Reorder.Group>
+    <>
+    <div className="container__buttons">
+
+      <div className="check__drag">
+        <p className="switch__title">Изменить порядок маршрута</p>
+        <Switch size="small" checked={drag} onChange={onChange} />
+      </div>
+      <div className="check__drag">
+        {/* <p className="switch__title">Начать поездку</p> */}
+        <Button type="primary" onClick={showModal} disabled={drag}>
+          Выехал
+        </Button>
+      </div>
+
+    </div>
+      
+      <Modal
+        open={open}
+        title="Внимание!"
+        onOk={handleOk}
+        onCancel={handleCancel}
+        footer={[
+          <Button key="back" onClick={handleCancel}>
+            Закрыть
+          </Button>,
+          <Button key="submit" type="primary" loading={loading} onClick={handleOk}>
+            Начать
+          </Button>,
+          
+        ]}
+      ><Result
+      status="warning"
+      style={{fontSize: '13px'}}
+      title="Нажимая кнопку 'Начать', клиенты получат уведомления, что вы выехали. Желаете начать маршрут?"
+      className="result__go"
+    />
+     
+
+      </Modal>
+
+      <Reorder.Group axis="y" onReorder={setItems} values={items}>
+        {items.map((item, index) => (
+          <Item key={item.id} item={item} index={index} drag={drag} />
+        ))}
+      </Reorder.Group>
+
+
+    </>
   );
 }
--- src/Item.js
+++ src/Item.js
@@ -1,25 +1,82 @@
 import * as React from "react";
+import { useState } from "react";
 import { useMotionValue, Reorder, useDragControls } from "framer-motion";
 import { useRaisedShadow } from "./use-raised-shadow";
 import { ReorderIcon } from "./Icon";
-
+import { Col, Row, Button, Modal } from "antd";
+import Icon, { InfoCircleOutlined, CheckOutlined } from "@ant-design/icons";
 
 export const Item = (props) => {
   const y = useMotionValue(0);
   const boxShadow = useRaisedShadow(y);
-  
   const dragControls = useDragControls();
 
+//Модалка инфо:
+  const [isModalOpenInfo, setIsModalOpenInfo] = useState(false);
+  const showModalInfo = () => {
+    setIsModalOpenInfo(true);
+  };
+  const handleOkInfo = () => {
+    setIsModalOpenInfo(false);
+  };
+  const handleCancelInfo = () => {
+    setIsModalOpenInfo(false);
+  };
+
+  //Модалка выполнено:
+  const [isModalOpenDone, setIsModalOpenDone] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const showModalDone = () => {
+    setIsModalOpenDone(true);
+  };
+  const handleOkDone = () => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+      setIsModalOpenDone(false);
+    }, 1000);
+  };
+  const handleCancelDone = () => {
+    setIsModalOpenDone(false);
+  };
+
   return (
-    <Reorder.Item
-      value={props.item}
-      id={props.item.id}
-      style={{ boxShadow, y }}
-      dragListener={true}
-      dragControls={dragControls}
-    ><p className="item_text_queue" >{props.index + 1}</p>
-      <span><p className="item_text" >{props.item.text}</p></span>
-      <ReorderIcon dragControls={dragControls} />
-    </Reorder.Item>
+    <>
+      <Reorder.Item value={props.item} id={props.item.id} style={{ boxShadow, y }} dragListener={props.drag} dragControls={dragControls}>
+        <Row justify="space-between" align="middle">
+          <Col span={2}>
+            <p className="item_text_queue">{props.index + 1}</p>
+          </Col>
+          <Col span={15}>
+            <p className="item_text">{props.item.text}</p>
+          </Col>
+          <Col span={7} className="col__end">
+            <CheckOutlined className="button__done" onClick={showModalDone} />
+            <InfoCircleOutlined className="button__info" onClick={showModalInfo} />
+            <ReorderIcon dragControls={dragControls} className="button__info" />
+          </Col>
+        </Row>
+      </Reorder.Item>
+      <Modal title="Информация о заказе:" open={isModalOpenInfo} onOk={handleOkInfo} onCancel={handleCancelInfo} cancelText="Отмена">
+        <p>Имя: Валентина Туманова</p>
+        <p>Телефон: 79846613788</p>
+        <p>Время заказа: 21 мая 2023 г. 3:15</p>
+        <p>Место покупки: ИНТЕРНЕТ-МАРКЕТПЛЕЙС ОЗОН ДВ</p>
+        <p>Комментарий: (НАЛ) OZON , ЗВОНОК ЗА ЧАС - 8 952 081 11 41 , доп. номер - 8 994 010 20 29 , Ботанический сад , частный дом</p>
+      </Modal>
+
+      <Modal title="Подтверждение" open={isModalOpenDone} cancelText="Отмена" onCancel={handleCancelDone} footer={[
+          <Button key="back" onClick={handleCancelDone}>
+            Закрыть
+          </Button>,
+          <Button key="submit" type="primary" loading={loading} onClick={handleOkDone}>
+            Выполнено!
+          </Button>,
+          
+        ]}>
+        <p>Вы уверены?</p>
+        
+      </Modal>
+    </>
   );
 };
