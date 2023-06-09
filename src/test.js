61494238a7b794357be8182c96df29dabde0a3c6
nikolay <saitov@impermebel.ru>
2023-06-09 17:20:37 +0300

    09.06

---
+++ src/test.js
@@ -0,0 +1,26 @@
+
+const courier = 
+  {
+    id: "kjdo37fhb4r78ewn",
+    started_order: false,
+    delivered: false,
+    courier_name: "Иванов Иван Иванович",
+    courier_phone: 89999999999,
+
+  }
+;
+const orders = [
+  {
+    id: 44889911,
+    queue_number: 1,
+    delivered: false,
+    order: 107644,
+    adress: "Владивосток, ул Адмирала Кузнецова, д 18, кв 426",
+    client_name: "Иванов Иван Иванович",
+    client_phone: 89999999999,
+    order_time: "21 мая 2023 г. 3:15",
+    order_shop: "ИНТЕРНЕТ-МАРКЕТПЛЕЙС ОЗОН ДВ",
+    comment: "(НАЛ) OZON , ЗВОНОК ЗА ЧАС - 8 952 081 11 41 , доп. номер - 8 994 010 20 29 , Ботанический сад , частный дом"
+
+  },
+];
